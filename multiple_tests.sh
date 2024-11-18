9e81dca8e12918b413eced99b7236b1baf9b2675
Matti de Beer <s2026994@ed.ac.uk>
2024-11-18 15:45:14 +0000

    new bash script to run multiple tests

--- multiple_tests.sh
+++ multiple_tests.sh
@@ -1,7 +1,15 @@
 #./vgg_38_custom.sh -b bn_res -l 1e-2
 
-./vgg_38_custom.sh -b bn_res -w 1e-1
+#./vgg_38_custom.sh -b bn_res -w 1e-1
 
-./vgg_38_custom.sh -b bn_res -w 1e-2
+#./vgg_38_custom.sh -b bn_res -w 1e-2
 
-./vgg_38_custom.sh -b bn_res -l 1e-3
+#./vgg_38_custom.sh -b bn_res -l 1e-3
+
+
+./vgg_38_custom.sh -b bn
+
+./vgg_38_custom.sh -b res 
+
+./vgg_38_custom.sh -b bn_res 
+
