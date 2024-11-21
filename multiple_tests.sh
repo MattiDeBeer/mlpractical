6a9ecc655cef978d9e1b0af936d80dd228867e4d
Matti de Beer <s2026994@ed.ac.uk>
2024-11-21 20:11:18 +0000

    Bash scripts for running multiple tests

--- multiple_tests.sh
+++ multiple_tests.sh
@@ -1,15 +1,8 @@
-#./vgg_38_custom.sh -b bn_res -l 1e-2
-
-#./vgg_38_custom.sh -b bn_res -w 1e-1
-
-#./vgg_38_custom.sh -b bn_res -w 1e-2
-
-#./vgg_38_custom.sh -b bn_res -l 1e-3
-
-
-./vgg_38_custom.sh -b bn
+./vgg_38_custom.sh -b bn_res
 
 ./vgg_38_custom.sh -b res 
+ 
+./vgg_38_custom.sh -b bn_res -l 1e-2
 
-./vgg_38_custom.sh -b bn_res 
+./vgg_38_custom.sh -b bn -l 1e-2
 
--- vgg_38_custom.sh
+++ vgg_38_custom.sh
@@ -1,10 +1,12 @@
-while getopts b:cl:w: flag
+
+while getopts b:cl:w:s: flag
 do
     case "${flag}" in
         b) block_type_arg=${OPTARG};;
         c) cpu='True';;
         l) learning_rate_arg=${OPTARG};;
 	w) weight_decay_arg=${OPTARG};;
+	s) start_num_arg=${OPTARG};; 
     esac
 done
 
@@ -18,25 +20,38 @@ fi
 if [ -z "$block_type_arg" ]
 then
     block_type="conv_block"
+    block_type_txt="_${block_type}"
 else
     block_type="conv_block_$block_type_arg"
+    block_type_txt="_${block_type}"
 fi
 
 if [ -z "$learning_rate_arg" ]
 then
     learning_rate="1e-3"
+    learning_rate_txt=""
 else
     learning_rate="$learning_rate_arg"
+    learning_rate_txt="_LR=${learning_rate}"
 fi
 
 if [ -z "$weight_decay_arg" ]
 then
     weight_decay="0"
+    weight_decay_txt=""
 else
     weight_decay="$weight_decay_arg"
+    weight_decay_txt="_WD=${weight_decay}"
 fi
 
-experiment_name="VGG_38_${block_type_arg}_lr${learning_rate}_wd${weight_decay}_experiment" 
+if [ -z "$start_num_arg" ]
+then
+    start_num="-1"
+else
+    start_num="$start_num_arg"
+fi
+
+experiment_name="VGG_38${block_type_txt}${learning_rate_txt}${weight_decay_txt}_experiment" 
 echo "Experiment name: ${experiment_name}"
 
 echo "using block type: $block_type";
@@ -44,4 +59,4 @@ echo "gpu: $gpu";
 echo "learning_rate: $learning_rate";
 echo "weight_decay: $weight_decay"
 
-python pytorch_mlp_framework/train_evaluate_image_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --num_stages 3 --num_blocks_per_stage 5 --experiment_name $experiment_name --use_gpu $gpu --num_classes 100 --block_type $block_type --continue_from_epoch -1 --learning_rate $learning_rate --weight_decay_coefficient $weight_decay
+python pytorch_mlp_framework/train_evaluate_image_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --num_stages 3 --num_blocks_per_stage 5 --experiment_name $experiment_name --use_gpu $gpu --num_classes 100 --block_type $block_type --continue_from_epoch $start_num --learning_rate $learning_rate --weight_decay_coefficient $weight_decay
